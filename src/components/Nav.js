13b1277170a5014e6e4a32b045f8edc47fc4af47
Gary Roberts <tallstreet@gmail.com>
2017-08-07 20:36:01 +0100

    Remove unneeded

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { graphql, gql } from "react-apollo";
-import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
